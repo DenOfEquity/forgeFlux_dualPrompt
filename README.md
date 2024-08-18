e08147f6995345f98ad9c62ef299de8af6bed03f
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-08-18 23:29:14 +0100

    sdxl too, minor fixes for edge case situations

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-## dual prompt extension for Flux in Forge webUI ##
+## multi-prompt extension for Flux and SDXL in Forge webUI ##
 ### intended as quick proof of concept only ###
+#### yet somehow updated already ####
 
 install:
 **Extensions** tab, **Install from URL**, use URL for this repo
 
 usage:
-1. Enable the extension: **Split Prompt** -> **Flux Enabled** checkbox
+1. Enable the extension: **Multi-prompt** -> **Enabled** checkbox
 2. use the Prompt textbox as normal
-3. prompt for CLIP first, tags
+3. prompt for first text encoder
 4. enter word **SPLIT**, use surrounding whitespace/newline as you like
-5. prompt for T5, natural language
+5. prompt for second text encoder
 6. Generate
 
+Flux (CLIP-L / T5) assumed best usage is tags for first prompt, natural language for second.
+SDXL (CLIP-L / CLIP-G) would typically be tags for both but depends on training: seems likely that same captions would be used for both CLIPs so use may be limited.
+
+update 1:
+* added sdXL, why not? In process, prepped code for possible future additions of SD3 / Hunyuan / others?.
+* changed unpatch location. Previously unpatched ASAP therefore wouldn't have applied during hires fix (only relevant if hires fix had new prompt which included SPLIT). In practice, unlikely to be relevant at all.
+* only patches if using appropriate models (Flux or sdXL). Previously always patched if extension enabled, which would have left the patch in place if using a model which didn't use the patched function which would have unpatched itself but couldn't.
+* Force clearance of cached conds when extension enabled/disabled.
+* for explanation of these egregious errors, see line 2 of this document.
--- scripts/fluxSplitprompt.py
+++ scripts/fluxSplitprompt.py
@@ -1,42 +1,59 @@
+from backend import memory_management
 from backend.diffusion_engine.flux import Flux
+from backend.diffusion_engine.sdxl import StableDiffusionXL
 import gradio
 import torch
 from modules import scripts
 
-from backend import memory_management
 
-class fluxSplitPrompt(scripts.Script):
+class forgeMultiPrompt(scripts.Script):
     sorting_priority = 0
-    glc_backup = None
-
+    glc_backup_flux = None
+    glc_backup_sdxl = None
+    clearConds = False
 
     def __init__(self):
-        fluxSplitPrompt.glc_backup = Flux.get_learned_conditioning
+        if forgeMultiPrompt.glc_backup_flux is None:
+            forgeMultiPrompt.glc_backup_flux = Flux.get_learned_conditioning
+        if forgeMultiPrompt.glc_backup_sdxl is None:
+            forgeMultiPrompt.glc_backup_sdxl = StableDiffusionXL.get_learned_conditioning
 
-    @torch.inference_mode()
-    def patched_glc(self, prompt: list[str]):
-        #   first, unpatch in case of error
-        Flux.get_learned_conditioning = fluxSplitPrompt.glc_backup
-        
-        memory_management.load_model_gpu(self.forge_objects.clip.patcher)
+    def splitPrompt (prompt, countTextEncoders):
+        promptTE1 = []
+        promptTE2 = []
+#        promptTE3 = []
 
-#        print (prompt)
-
-        #   make 2 prompt lists, split each prompt in original list based on 'SPLIT'
-        CLIPprompt = []
-        T5prompt = []
         for p in prompt:
             splitPrompt = p.split('SPLIT')
-            if len(splitPrompt) >= 2:
-                CLIPprompt.append(splitPrompt[0].strip())
-                T5prompt.append(splitPrompt[1].strip())
-            else:
-                CLIPprompt.append(p)
-                T5prompt.append(p)
+            
+            countSplits = min (countTextEncoders, len(splitPrompt))
+            match countSplits:
+#                case 3:         #   sd3, included for future proofing
+#                    promptTE1.append(splitPrompt[0].strip())
+#                    promptTE2.append(splitPrompt[1].strip())
+#                    promptTE3.append(splitPrompt[2].strip())
+                case 2:         #   sdxl, flux, hunyuan future proofing or SD3 with incomplete SPLITs
+                    promptTE1.append(splitPrompt[0].strip())
+                    promptTE2.append(splitPrompt[1].strip())
+#                    promptTE3.append(p)
+                case 1:         #   sd1,    or Any if SPLIT not used
+                    promptTE1.append(p)
+                    promptTE2.append(p)
+#                    promptTE3.append(p)
+                case _:
+                    promptTE1.append(p)
+                    promptTE2.append(p)
+#                    promptTE3.append(p)
 
-#        print (CLIPprompt)
-#        print (T5prompt)
-        
+        return promptTE1, promptTE2#, promptTE3
+
+    @torch.inference_mode()
+    def patched_glc_flux(self, prompt: list[str]):
+        memory_management.load_model_gpu(self.forge_objects.clip.patcher)
+
+        #   make 2 prompt lists, split each prompt in original list based on 'SPLIT'
+        CLIPprompt, T5prompt = forgeMultiPrompt.splitPrompt (prompt, 2)
+
         cond_l, pooled_l = self.text_processing_engine_l(CLIPprompt)
         cond_t5 = self.text_processing_engine_t5(T5prompt)
         cond = dict(crossattn=cond_t5, vector=pooled_l)
@@ -50,8 +67,60 @@ class fluxSplitPrompt(scripts.Script):
 
         return cond
 
+
+    @torch.inference_mode()
+    def patched_glc_sdxl(self, prompt: list[str]):
+        memory_management.load_model_gpu(self.forge_objects.clip.patcher)
+
+        #   make 2 prompt lists, split each prompt in original list based on 'SPLIT'
+        CLIPLprompt, CLIPGprompt = forgeMultiPrompt.splitPrompt (prompt, 2)
+        
+        cond_l = self.text_processing_engine_l(CLIPLprompt)
+        cond_g, clip_pooled = self.text_processing_engine_g(CLIPGprompt)
+
+        #   conds get concatenated later, so sizes of dimension 1 must match
+        #   padding with zero
+        pad = cond_g.size(1) - cond_l.size(1)
+        if pad > 1:
+            padding = (0,0, 0, pad, 0,0)
+            cond_l = torch.nn.functional.pad (cond_l, padding, mode='constant', value=0)
+        elif pad < 1:
+            padding = (0,0, 0, -pad, 0,0)
+            cond_g = torch.nn.functional.pad (cond_g, padding, mode='constant', value=0)
+
+        width = getattr(prompt, 'width', 1024) or 1024
+        height = getattr(prompt, 'height', 1024) or 1024
+        is_negative_prompt = getattr(prompt, 'is_negative_prompt', False)
+
+        crop_w = 0
+        crop_h = 0
+        target_width = width
+        target_height = height
+
+        out = [
+            self.embedder(torch.Tensor([height])), self.embedder(torch.Tensor([width])),
+            self.embedder(torch.Tensor([crop_h])), self.embedder(torch.Tensor([crop_w])),
+            self.embedder(torch.Tensor([target_height])), self.embedder(torch.Tensor([target_width]))
+        ]
+
+        flat = torch.flatten(torch.cat(out)).unsqueeze(dim=0).repeat(clip_pooled.shape[0], 1).to(clip_pooled)
+
+        force_zero_negative_prompt = is_negative_prompt and all(x == '' for x in prompt)
+
+        if force_zero_negative_prompt:
+            clip_pooled = torch.zeros_like(clip_pooled)
+            cond_l = torch.zeros_like(cond_l)
+            cond_g = torch.zeros_like(cond_g)
+
+        cond = dict(
+            crossattn=torch.cat([cond_l, cond_g], dim=2),
+            vector=torch.cat([clip_pooled, flat], dim=1),
+        )
+
+        return cond
+
     def title(self):
-        return "Split Prompt"
+        return "Multi-prompt // for Flux and sdXL"
 
     def show(self, is_img2img):
         # make this extension visible in both txt2img and img2img tab.
@@ -59,21 +128,47 @@ class fluxSplitPrompt(scripts.Script):
 
     def ui(self, *args, **kwargs):
         with gradio.Accordion(open=False, label=self.title()):
-            enabled_flux = gradio.Checkbox(value=False, label='Flux Enabled', info='Flux split prompt: 1st prompt tags, 2nd prompt natural language. Split with "SPLIT".')
+            enabled = gradio.Checkbox(value=False, label='Enabled', info='separator keyword: SPLIT')
 
         self.infotext_fields = [
-            (enabled_flux, lambda d: d.get("fsp_enabled", False)),
+            (enabled, lambda d: d.get("fmp_enabled", False)),
         ]
 
-        return [enabled_flux]
+        def clearCondCache ():
+            forgeMultiPrompt.clearConds ^= True      #   if False, set to True; if True then next Generate hasn't happened so safe to reset to False
+
+        enabled.change (fn=clearCondCache, inputs=[], outputs=[])
+
+        return [enabled]
 
     def process(self, params, *script_args, **kwargs):
-        enabled_flux = script_args[0]
+        enabled = script_args[0]
 
-        if enabled_flux:
+        #   clear conds if usage has changed - must do this even if extension has been disabled
+        if forgeMultiPrompt.clearConds == True:
+            params.clear_prompt_cache()
+            forgeMultiPrompt.clearConds = False
+
+        #   detect sdxl or flux - is there a better way? 
+        isMPModel = not ((params.sd_model.is_sd1 == True) or (params.sd_model.is_sd2 == True))
+
+        if enabled and isMPModel:
             params.extra_generation_params.update({
-                "fsp_enabled"        :   enabled_flux,
+                "fmp_enabled"        :   enabled,
             })
-            Flux.get_learned_conditioning = fluxSplitPrompt.patched_glc
+            if params.sd_model.is_sdxl == True:
+                StableDiffusionXL.get_learned_conditioning = forgeMultiPrompt.patched_glc_sdxl
+            else:
+                Flux.get_learned_conditioning = forgeMultiPrompt.patched_glc_flux
+
+        return
+
+    def postprocess(self, params, processed, *args):
+        enabled = args[0]
+        if enabled:
+            if params.sd_model.is_sdxl == True:
+                StableDiffusionXL.get_learned_conditioning = forgeMultiPrompt.glc_backup_sdxl
+            else:
+                Flux.get_learned_conditioning = forgeMultiPrompt.glc_backup_flux
 
         return
